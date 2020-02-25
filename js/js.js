6e5ef9f569f8e8512f87668cefdd6a223c1d212f
Andrew <taf_397@yahoo.com>
2020-02-25 11:32:06 +0300

    Кнопка ВВЕРХ

--- js/js.js
+++ js/js.js
@@ -1,3 +1,14 @@
 $(function () {
-baguetteBox.run('.gallery');
+    baguetteBox.run('.gallery');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    })
+});
+$('.scrollToTop').click(function () {
+    $('html, body').animate({scrollTop: 0}, 800);
+    return false
 });
